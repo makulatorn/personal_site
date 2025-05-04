f7c18605404388939e1aae191993bcc39df1b751
sasha <sasha.friis@icloud.com>
2025-05-04 16:09:57 +0200

    fixed buggy menu + fixed minor styling
    aligned ASCII title, subtitle and main

--- src/utils/Nav.svelte
+++ src/utils/Nav.svelte
@@ -1,14 +1,29 @@
+<script>
+	let isOpen = false;
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
+</script>
+
 <nav>
-    <button class="burger">X</button>
-    <ul class="nav-tabs">
-        <li><a href="/">Home</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/resources">Resources</a></li>
-    </ul>
-    <aside class="side-menu">
-        <ul class="side-menu-list">
-            <li><a id="Github" href="https://github.com/makulatorn" target="_blank">Github</a></li>
-            <li><a id="Bandcamp" href="https://makulator.bandcamp.com/" target="_blank">Bandcamp</a></li>
-        </ul>
-    </aside>
+	<button class="burger" on:click={toggleMenu}>
+		{#if isOpen} ✕ {/if}
+		{#if !isOpen} ☰ {/if}
+	</button>
+
+	<ul class="nav-tabs {isOpen ? 'open' : ''}">
+		<li><a href="/">Home</a></li>
+		<li><a href="/projects">Projects</a></li>
+		<li><a href="/resources">Resources</a></li>
+	</ul>
+
+	<aside class="side-menu {isOpen ? 'open' : ''}">
+		<ul class="side-menu-list">
+			<li><a id="Github" href="https://github.com/makulatorn" target="_blank">Github</a></li>
+			<li><a id="Bandcamp" href="https://makulator.bandcamp.com/" target="_blank">Bandcamp</a></li>
+		</ul>
+	</aside>
 </nav>
+
+
