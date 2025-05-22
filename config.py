c830d72b23b6eb7fb6a806dce57dfcbe21a8e70a
sasha <sasha.friis@icloud.com>
2025-05-22 22:37:16 +0200

    made a whoopsie

--- config.py
+++ config.py
@@ -10,4 +10,3 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
