54a1a2ab00d1be8adbc44261cdb73b519ec79668
sasha <sasha.friis@icloud.com>
2025-07-23 02:19:52 +0200

    so much stuff went wrong using netlify

--- deploy/static/js/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerBtn = document.getElementById('burgerBtn');
-    const navTabs = document.getElementById('navTabs');
-
-    burgerBtn.addEventListener('click', () => {
-        const isOpen = navTabs.classList.toggle('open');
-        burgerBtn.textContent = isOpen ? '✕' : '☰';
-    });
-});
