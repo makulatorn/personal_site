b737604756d6951cd6384d271a92cb682e0216b8
sasha <sasha.friis@icloud.com>
2025-07-23 02:25:33 +0200

    idk

--- layout.php
+++ layout.php
@@ -37,7 +37,7 @@
             <nav>
                 <button class="burger" id="burgerBtn">☰</button>
                 <ul class="nav-tabs" id="navTabs">
-                    <li><a href="/personal_site_php/public/">Home</a></li>
+                    <li><a href="/personal_site_php/public/home">Home</a></li>
                     <li><a href="/personal_site_php/public/projects">Projects</a></li>
                     <li><a href="/personal_site_php/public/contact">Contact</a></li>
                 </ul>
